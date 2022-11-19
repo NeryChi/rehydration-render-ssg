27f5349666411ec6ef2925de22bc1222fd499a8d
Nery Lopez <nerya.lopezs.18@gmail.com>
2022-11-19 00:55:59 -0600

    Seccion de usuario

--- app/checkUser.jsx
+++ app/checkUser.jsx
@@ -1,49 +1,36 @@
 import { useEffect, useState } from "react";
 import Profile from "./profile";
 
-let user = ''
-
 const CheckUser = () => {
 
+  const [user, setUser] = useState()
   const [hasMounted, setHasMounted] = useState(false)
 
   useEffect(() => {
-
     setHasMounted(true)
-    console.log('no entra al use effect')
-    
   }, [])
 
-  if(!hasMounted){
-    return null
-  }
-
   const getUser = () => {
-    console.log('entro getUser()')
     fetch('https://randomuser.me/api/').then((res) => res.json()).then((data) => dataUser(data)).catch(err => console.log(err))
   }
 
   const dataUser = (data) => {
-    console.log('entro dataUser()')
-    
-    user = [data.results[0].name, data.results[0].phone, data.results[0].dob, data.results[0].picture]
-    console.log(user)
+    setUser([data.results[0].name, data.results[0].phone, data.results[0].dob, data.results[0].location, data.results[0].email, data.results[0].picture])
   }
 
-  const vari = getUser()
-
-  if(user){
-    console.log('entra al if')
+  if (user) {
     document.getElementById('check').className = 'bg-slate-900 opacity-[97%] hover:opacity-[97%] grid grid-cols-1 grid-rows-2 flex justify-center items-center duration-300 '
-    return (<Profile user={user}/>)
-  } else{
-    console.log('no entra al if')
+    return <Profile user={user} />
   }
 
-  return(
-    <div>
-      <img src="/delete.png" alt="oh" />
-    </div>
+  if (!hasMounted) {
+    getUser()
+    document.getElementById('check').className = 'bg-slate-900 opacity-[97%] hover:opacity-[97%] grid grid-cols-1 grid-rows-2 flex justify-center items-center duration-300 '
+    return <h1>hola</h1>
+  }
+
+  return (
+    <Profile user={' / '} />
   )
 }
 
--- app/profile.jsx
+++ app/profile.jsx
@@ -1,8 +1,29 @@
-const Profile = ({user}) => {
-  return(
-    <div className="flex h-[100%] grid grid-flow-row-4">
-      <p className="ml-[10%] self-center text-white">{`Nombre:  ${user[0].first} ${user[0].last}`}</p>
-      <p className="ml-[10%] self-center text-white">{`Edad:  ${user[2].age}`}</p>
+import Image from "next/image"
+
+const Profile = ({ user }) => {
+
+  console.log(user)
+
+  let info = [
+    user[0].first ? user[0].first : '',
+    user[0].last ? user[0].last : '',
+    user[1] ? user[1] : '',
+    user[2].age ? user[2].age + ' años' : '',
+    user[3] ? user[3].city + ', ' + user[3].country : '',
+    user[5] ? user[5].large : '/trasparent.png'
+  ]
+
+  return (
+    <div className="grid grid-cols-3">
+      <div className="grid col-span-2">
+        <p className="ml-[10%] self-center text-white flex">Nombre: <p className="text-[#53AFEE] ml-[0.7vw]">{`${info[0]} ${info[1]}`}</p></p><br />
+        <p className="ml-[10%] self-center text-white flex">Teléfono: <p className="text-[#53AFEE] ml-[0.7vw]">{info[2]}</p></p><br />
+        <p className="ml-[10%] self-center text-white flex">Edad: <p className="text-[#53AFEE] ml-[0.7vw]">{info[3]}</p></p><br />
+        <p className="ml-[10%] self-center text-white flex">Dirección: <p className="text-[#53AFEE] ml-[0.7vw]">{info[4]}</p></p><br />
+      </div>
+      <img src={info[5]} alt="User" className="w-[75%] object-cover rounded-[50%] border-4 border-green-500 row-span-2 " />
+
+
     </div>
   )
 }
